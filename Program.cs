5cacbc9f527af53ac34fc81143bbc562a7302ccc
Boichenko Sergei <boychenkosergeya@gmail.com>
2023-09-20 22:59:45 +0300

    Commit3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using ДЗ_3._Console_program_with_objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ДЗ_3._Console_program_with_objects.Classes;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UserInputDataProvider userInputDataProvider = new UserInputDataProvider();
+            User newUser = new User(userInputDataProvider.GetUserAge());
+
+            newUser.FirstName = userInputDataProvider.GetUserName();
+            newUser.LastName = userInputDataProvider.GetLastName();
+            newUser.ChoiseGend = userInputDataProvider.ChoiseGender();
+
+
+            var getInfoForUser = newUser.ToString();
+            Console.WriteLine(getInfoForUser);
+        }
+    }
+}
+
